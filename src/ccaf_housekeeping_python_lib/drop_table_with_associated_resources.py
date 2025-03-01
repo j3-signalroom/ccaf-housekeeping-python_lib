b0f3c8942cfc89ffe816a1128092591a711b1892
Jeffrey Jonathan Jennings (J3) <j3@signalroom.io>
2025-02-28 19:01:57 -0500

    See #40.  Update lib.

--- src/ccaf_housekeeping_python_lib/drop_table_with_associated_resources.py
+++ src/ccaf_housekeeping_python_lib/drop_table_with_associated_resources.py
@@ -1,4 +1,4 @@
-from cc_clients_python_lib.flink_sql_client import FlinkSqlClient
+from cc_clients_python_lib.flink_client import FlinkClient
 from cc_clients_python_lib.common import HttpStatus
 
 
@@ -12,10 +12,12 @@ __status__     = "dev"
 
 class DropTableWithAssociatedResources():
     def __init__(self, flink_sql_config: dict):
-        # Instantiate the FlinkSqlClient classs.
-        self.flink_client = FlinkSqlClient(flink_sql_config)
+        # Instantiate the Flinklient classs.
+        self.flink_client = FlinkClient(flink_sql_config)
 
     def drop_table(self, catalog_name: str, database_name: str, table_name: str):
-        http_status_code, error_message, response = flink_client.submit_statement("drop-statement",
-                                                                              "DROP TABLE IF EXISTS hello;", 
-                                                                              {"sql.current-catalog": catalog_name, "sql.current-database": database_name})
+        http_status_code, error_message, response = self.flink_client.get_statement_list()
+
+        http_status_code, error_message, response = self.flink_client.submit_statement(f"drop-{table_name}",
+                                                                                       f"DROP TABLE IF EXISTS {table_name};", 
+                                                                                       {"sql.current-catalog": catalog_name, "sql.current-database": database_name})
